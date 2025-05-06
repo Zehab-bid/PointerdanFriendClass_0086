7e3d4e614ce0ad463cf538edd7dcada4ade7e7ad
Zehab-bid <zehabidha152gmail.com>
2025-05-06 15:22:57 +0700

    end pointer

--- pointer1.cpp
+++ pointer1.cpp
@@ -13,8 +13,15 @@ class mahasiswa
 
 int main()
 {
-    mahasiswa mhs(1); //objek mhs
+    mahasiswa mhs{1}; //objek mhs
     mhs.showNim(); //member acces operator
 
     mahasiswa &refMhs = mhs; //pointer refence refMhs
+    refMhs.nim = 2; // member acces operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer deference pMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+    return 0;
 }
