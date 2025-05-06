fb9f5e9442e5641218a3f5442985650977f807fb
Zehab-bid <zehabidha152gmail.com>
2025-05-06 15:42:16 +0700

    proges 2

--- FriendClass1.cpp
+++ FriendClass1.cpp
@@ -20,4 +20,24 @@ class siswa
     public:
         void setId(int pId);
         void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("joko susilo");
+    siswa joko_siswa;
+    
 }
